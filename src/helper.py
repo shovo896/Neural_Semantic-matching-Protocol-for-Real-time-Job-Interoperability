4bf9fe9914a564011b9a65e0630c820aa0f1b789
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-09-25 18:46:07 +0600

    bcdhbxjm

--- src/helper.py
+++ src/helper.py
@@ -8,6 +8,7 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 client = OpenAI(api_key=OPENAI_API_KEY)
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 
 '''def extract_text_from_pdf(uploaded_file):
