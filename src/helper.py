a859bcfb0b6f580d460f68a286aee260cbc15e76
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-09-25 18:24:20 +0600

    hbjh

--- src/helper.py
+++ src/helper.py
@@ -22,6 +22,10 @@ load_dotenv()
     
     
     doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
-    
+    text = "" 
+    for page in doc: 
+        text += page.get_text()
+    doc.close()
+    return text
 
 
