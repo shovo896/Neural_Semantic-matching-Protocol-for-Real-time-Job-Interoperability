7f2fc6e0e5817566c7d3968f1760515b2075e4c1
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-09-25 18:46:29 +0600

    nh knm

--- src/helper.py
+++ src/helper.py
@@ -7,7 +7,7 @@ from openai import OpenAI
 load_dotenv() 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-client = OpenAI(api_key=OPENAI_API_KEY)
+
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 
