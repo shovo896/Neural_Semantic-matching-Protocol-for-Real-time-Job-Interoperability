9bdfb2f9ff555bef7589fc89c0f9e56670c9bdb2
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-09-25 17:41:11 +0600

    chvhj

--- src/helper.py
+++ src/helper.py
@@ -5,3 +5,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+def extract_text_from_pdf(uploaded_file):
+    """
+    Extracts text from a PDF file.
+
+    Args:
+        uploaded_file: The uploaded PDF file.
+        """ 
+    text = "" 
+    try: 
+        with fitz.open(uploaded_file) as pdf: 
+            for page in pdf: 
+                text += page.get_text() 
+    except Exception as e: 
+        print(f"Error extracting text from {uploaded_file}: {e}")
+    return text
