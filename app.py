ae88cae3c0851b7ce858dbf646afd2b893e5008a
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-09-26 10:58:20 +0600

    jkmnl

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as 
-from src.helper import extract_text_from_pdf 
+from src.helper import extract_text_from_pdf ask_openai
 
 
 
@@ -12,4 +12,16 @@ if uploaded_file :
         text = extract_text_from_pdf(uploaded_file)
         st.success("Text extracted successfully!")
         st.text_area("Extracted Text", text, height=300)
+        
+    with st.spinner("summarizing your resume...."): 
+        summary = ask_openai(f"Summarize the following resume text:\n{text}", model="gpt-oss-128B", temperature=0.7, max_tokens=500)
+        st.success("Resume summarized successfully!")
+        
+    with st.spinner("Finding skill gaps on your resume.... "):
+        skill_gaps = ask_openai(f"Identify skill gaps in the following resume text:\n{text}", model="gpt-oss-128B", temperature=0.7, max_tokens=500)
+        st.success("Skill gaps identified successfully!")
+        st.text_area("Identified Skill Gaps", skill_gaps, height=300)
+        
+        
+    
         
