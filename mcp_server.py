53bcfbcc5a2b4e995253a046d8f951207a7f476b
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-09-26 14:30:44 +0600

    kklm

--- mcp_server.py
+++ mcp_server.py
@@ -1,3 +1,24 @@
 from mcp.server.fastmcp import FastMCP
 from src.job_api import fetch_linkedin_job, fetch_naukri_job
 
+
+mcp = FastMCP(
+    title="Job Search Application",
+    description="This application allows you to search for jobs, upload your resume, and get job recommendations based on your skills and experience.",
+    theme="light",
+    layout="wide",
+    favicon=":mag:",
+)
+
+
+@mcp.tool()
+
+async def fetch_linkedin_jobs(listofkeywords):
+    return fetch_linkedin_job(listofkeywords, location="Bangladesh", rows=60)
+
+@mcp.tool()
+
+async def fetch_naukri_jobs(listofkeywords):
+    return fetch_naukri_job(listofkeywords, location="Bangladesh", rows=60)
+
+
